2e3be12d9f22b48071868b922788eba65e1aad82
gekomad <giuseppe.cannella@gmail.com>
2021-05-23 17:09:29 +0200

    v2.4

--- src/namespaces/constants.h
+++ src/namespaces/constants.h
@@ -27,7 +27,7 @@
 using namespace std;
 namespace constants {
     
-    static const string NAME = "Cinnamon 2.4.0-75.3%";
+    static const string NAME = "Cinnamon 2.4";
     static const string STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     static constexpr int BLACK = 0;
     static constexpr int WHITE = 1;
