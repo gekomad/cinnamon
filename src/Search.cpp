49a8a057fd767d187cdb7df0fecd70b90664a6ab
gekomad <giuseppe.cannella@gmail.com>
2024-04-16 08:31:45 +0200

    1

--- src/Search.cpp
+++ src/Search.cpp
@@ -129,6 +129,15 @@ int Search::qsearch(int alpha, const int beta, const uchar promotionPiece, const
         alpha = score;
     }
 
+    /// **************Delta Pruning ****************
+    bool fprune = false;
+    int fscore;
+    if ((fscore = score + (promotionPiece == NO_PROMOTION ? VALUEQUEEN : 2 * VALUEQUEEN)) < alpha) {
+        fprune = true;
+    }
+    /// ************ end Delta Pruning *************
+    if (score > alpha) alpha = score;
+
     incListId();
 
     u64 friends = board::getBitmap<side>(chessboard);
@@ -152,6 +161,13 @@ int Search::qsearch(int alpha, const int beta, const uchar promotionPiece, const
             continue;
         }
 
+        /// **************Delta Pruning ****************
+        if (fprune && fscore + PIECES_VALUE[move->capturedPiece] <= alpha) {
+            INC(nCutFp);
+            takeback(move, oldKey, oldEnpassant, false);
+            continue;
+        }
+        /// ************ end Delta Pruning *************
         int val = -qsearch<X(side)>(-beta, -alpha, move->promotionPiece, depth - 1);
         score = max(score, val);
         takeback(move, oldKey, oldEnpassant, false);
