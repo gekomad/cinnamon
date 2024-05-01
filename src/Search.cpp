e7e14cc28efd345faa7f544cc8fb085668bbbb40
gekomad <giuseppe.cannella@gmail.com>
2024-05-01 12:13:53 +0200

    internal ID

--- src/Search.cpp
+++ src/Search.cpp
@@ -309,6 +309,13 @@ int Search::search(const int depth, int alpha, const int beta, _TpvLine *pline, 
         return hashValue;
     }
 
+    if (depth + extension > 3) {
+        _TpvLine newLine1;
+        newLine1.cmove = 0;
+        const int sc = searchLambda(&newLine1, (depth + extension) / 2, -beta, -alpha, nullptr);
+        Hash::_Thash data(zobristKeyR, sc, depth, newLine1.argmove[0].from, newLine1.argmove[0].to, Hash::hashfBETA);
+        hashItem = data.data;
+    }
     /// ********** end hash ***************
 
     if (!(numMoves % 2048)) setRunning(checkTime());
