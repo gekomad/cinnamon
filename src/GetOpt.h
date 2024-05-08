8ab502324e5934562fb239ec38a9da6452a5e451
gekomad <giuseppe.cannella@gmail.com>
2024-05-08 17:27:08 +0200

    bench 555565

--- src/GetOpt.h
+++ src/GetOpt.h
@@ -235,16 +235,17 @@ public:
             const auto start1 = std::chrono::high_resolution_clock::now();
             for (int i = 0; fens[i]; i++) {
                 it.loadFen(fens[i]);
-                searchManager.setMaxTimeMillsec(100);
+                it.setMaxDepth(9);
                 it.run();
-                // int a = searchManager.getTotMoves();
                 totMoves += searchManager.getTotMoves();
             }
             const auto end1 = std::chrono::high_resolution_clock::now();
             const auto totTime = Time::diffTime(end1, start1) + 1;
 
             printf("===========================\n"
-                   "OVERALL:                                         1892937 nodes      %d nps\n", totMoves / (totTime / 1000));
+                   "Total time (ms) : %d\n"
+                   "Nodes searched  : %d\n"
+                   "Nodes/second    : %d\n", totTime, totMoves, totMoves / (totTime / 1000));
             exit(0);
         }
         if (!(argc > 1 && !strcmp("-puzzle_epd", argv[1])))
